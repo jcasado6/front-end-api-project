3a690552b60d83ada1a6340dd4647d87d4f694fd
Jose Casado <jcasado6@yahoo.com>
2019-12-27 11:02:34 -0500

    created methods and binded them

--- src/CreateBrewery.js
+++ src/CreateBrewery.js
@@ -6,22 +6,91 @@ class CreateBrewery extends Component {
 
         this.state = {
             name: " ",
-            brewery_type : " ",
+            brewery_type: " ",
             street: " ",
             city: " ",
             state: " ",
             postal_code: " ",
             phone: " "
         }
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeBreweryType = this.onChangeBreweryType.bind(this);
+        this.onChangeStreet = this.onChangeStreet.bind(this);
+        this.onChangeCity = this.onChangeCity.bind(this);
+        this.onChangeState = this.onChangeState.bind(this);
+        this.onChangePostalCode = this.onChangePostalCode.bind(this);
+        this.onChangePhone = this.onChangePhone.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
-        render() {
-            return (
-                <div>
-                    <p>Create</p>
-                </div>
-            )
-        }
-    }
+
+
+onChangeName(evt) {
+    this.setState({
+        name: evt.target.value
+    })
+}
+
+onChangeBreweryType(evt) {
+    this.setState({
+        brewery_type: evt.target.value
+    })
+}
+onChangeStreet(evt) {
+    this.setState({
+        street: evt.target.value
+    })
+}
+onChangeCity(evt) {
+    this.setState({
+        city: evt.target.value
+    })
+}
+onChangeState(evt) {
+    this.setState({
+        state: evt.target.value
+    })
+}
+onChangePostalCode(evt) {
+    this.setState({
+        postal_code: evt.target.value
+    })
+}
+onChangePhone(evt) {
+    this.setState({
+        phone: evt.target.value
+    })
+}
+
+onSubmit(evt) {
+    evt.preventDefault()
+    console.log('submitted')
+    console.log(`name: ${this.state.name}`)
+    console.log(`brewery_type: ${this.state.brewery_type}`)
+    console.log(`street: ${this.state.street}`)
+    console.log(`city: ${this.state.city}`)
+    console.log(`state: ${this.state.state}`)
+    console.log(`postal_code: ${this.state.postal_code}`)
+    console.log(`phone: ${this.state.phone}`)
+
+    this.setState({
+        name: " ",
+        brewery_type: " ",
+        street: " ",
+        city: " ",
+        state: " ",
+        postal_code: " ",
+        phone: " "
+    })
+}
+
+render() {
+    return (
+        <div>
+            <p>Create</p>
+        </div>
+    )
+}
+}
 
 
 
