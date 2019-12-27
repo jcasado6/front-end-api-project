dc7de13e68057c988fec1a4fa5e5d884db585335
Jose Casado <jcasado6@yahoo.com>
2019-12-26 19:35:12 -0500

    cant fetch my backend

--- src/App.js
+++ src/App.js
@@ -5,32 +5,49 @@ import BreweryList from './BreweryList';
 import CreateBrewery from './CreateBrewery';
 import EditBrewery from './EditBrewery'
 
-
+const proxyurl = "https://cors-anywhere.herokuapp.com/"
+const url = 'http://localhost:8080/'
 class App extends Component {
-  render () {
-  return (
-    <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-           
-            <Link to="/" className="navbar-brand">DC Brewery Tracker</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Breweries</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Brewery</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={BreweryList} />
-          <Route path="/edit/:id" component={EditBrewery} />
-          <Route path="/create" component={CreateBrewery} />
-    </div>
-  );
+  constructor () {
+    super ()
+    this.state = {
+      beerData: []
+    }
+  }
+  componentDidMount(){
+    fetch (proxyurl + url)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      this.setState({beerData: res})
+      console.log(this.state.countryData)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+          <Link to="/" className="navbar-brand text-white">DC Brewery Tracker</Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link text-white">Breweries</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link text-white">Create Brewery</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Route path="/" exact component={BreweryList} />
+        <Route path="/edit/:id" component={EditBrewery} />
+        <Route path="/create" component={CreateBrewery} />
+      </div>
+    );
+  }
 }
-}
+
 
 export default App;
--- src/CreateBrewery.js
+++ src/CreateBrewery.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class CreateBrewery extends Component {
-    render() {
-        return (
-            <div>
-                <p>Create</p>
-            </div>
-        )
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: " ",
+            brewery_type : " ",
+            street: " ",
+            city: " ",
+            state: " ",
+            postal_code: " ",
+            phone: " "
+        }
     }
-}
+        render() {
+            return (
+                <div>
+                    <p>Create</p>
+                </div>
+            )
+        }
+    }
+
 
 
 export default CreateBrewery
